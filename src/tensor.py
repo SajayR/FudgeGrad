8dc9d13d75771e90be3976f76ac08f48bf0d88b9
SajayR <techrend08@gmail.com>
2025-02-03 18:47:53 +0530

    Adding backward

--- src/tensor.py
+++ src/tensor.py
@@ -1,8 +1,9 @@
 import numpy as np
+
 class Tensor():
     def __init__(self, shape: tuple=None, dtype=float, value: np.ndarray=None, requires_grad: bool=False, parents: tuple=None, gen_op: str=None) -> None:
         assert shape is not None or value is not None, "Either shape or data has to be passed for tensor creation"
-        assert ((shape is not None) and (value is None)) or ((shape is None) and (value is not None))
+        assert ((shape is not None) and (value is None)) or ((shape is None) and (value is not None)), "Only one of value or shape can be defined. If a numpy array is provided as the value, the shape will be replicated."
         if value is not None:
             assert isinstance(value, np.ndarray)
             self.data = value.copy()
@@ -14,8 +15,12 @@ class Tensor():
         self.requires_grad = requires_grad
         if parents is not None: self.parents = parents
         if gen_op is not None: self.gen_op = gen_op
-    def __repr__(self) -> str: return(f"Tensor Value: {self.data}")
-    def __add__(self, additive: "Tensor | float"):
+
+        if self.requires_grad:
+            self.grad = Tensor(value=np.zeros(self.shape))
+
+    def __repr__(self) -> str: return(f"{self.data}")
+    def __add__(self, additive: "Tensor | float"): #float carries no grad
         if isinstance(additive, Tensor):
             parents=(self, additive)
             return Tensor(value=np.add(self.data, additive.data), parents=parents, gen_op="Add", requires_grad=(self.requires_grad or additive.requires_grad))
